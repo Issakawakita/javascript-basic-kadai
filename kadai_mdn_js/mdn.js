9a4742344f4802f9a0b30d2e9c7daf7f65cf576f
Issakawakita <163989379+Issakawakita@users.noreply.github.com>
2024-04-24 18:47:07 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+// 年・月・日・曜日を取得
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const date = today.getDate();
+const day = today.getDay();
+
+console.log(`${year}年${month}月${date}日`);
